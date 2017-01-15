5f3a03be1bab15b844af24c755bed60816f94b08
ShaharHaramatiC <petunia.parpar@gmail.com>
2017-01-15 14:20:27 +0200

    update store

--- src/modules/auth/auth.module.js
+++ src/modules/auth/auth.module.js
@@ -1,10 +1,12 @@
 export const SIGN_IN = 'auth/SIGN_IN';
 export const SIGN_OUT = 'auth/SIGN_OUT';
+export const UPDATE_USER_SETTINGS = 'auth_modules/UPDATE_USER_SETTINGS';
 
 const state = {
   isLoggedIn: !!localStorage.getItem('token'),
   user: JSON.parse(localStorage.getItem('user')),
-  userLatestMeals: []
+  userLatestMeals: [],
+  settings: {'pushTimer': '1 hour', 'lang': 'en'}
 };
 
 const mutations = {
@@ -17,6 +19,11 @@ const mutations = {
   },
   latestMeals(state , payload){
     state.userLatestMeals = payload;
+  },
+  
+  [UPDATE_USER_SETTINGS](state, settings){
+    console.log('auth.modules.js: UPDATE_USER_SETTINGS mutations');
+    state.settings = settings;
   }
 }
 
@@ -24,19 +31,19 @@ const actions = {
   getLatestMeals({commit}){
     //TODO get from server
     let latestMeals = [
-    {
-      foods: ['meat','bread'],
-      time: 123123123,
-      _id: 123123,
-      userId: 1
-    },
-    {
-      foods: ['Milk','nuts'],
-      time: 124124124,
-      _id: 123123,
-      userId: 1
-    }
-  ];
+      {
+        foods: ['meat','bread'],
+        time: 123123123,
+        _id: 123123,
+        userId: 1
+      },
+      {
+        foods: ['Milk','nuts'],
+        time: 124124124,
+        _id: 123123,
+        userId: 1
+      }
+    ];
 
   commit('latestMeals' , latestMeals);
 
@@ -53,11 +60,11 @@ const actions = {
       resolve({msg: 'Added meal successful'});  
     })
   },
-  updateSettings( _ , settings){
-    //todo update db
-    return new Promise(resolve => {
-      resolve({msg: 'Changed settings successful'});  
-    })
+
+  updateSettings( state , settings){
+    //update the state
+    state.commit(UPDATE_USER_SETTINGS, settings);      
+    // console.log('1: auth.modules.js updateSettings: settings', settings);  
   }
 };
 
@@ -66,7 +73,7 @@ const getters = {
   isLoggedIn: state => state.isLoggedIn,
   user: state => state.user,
   userLatestMeals: state => state.userLatestMeals
-};
+}; 
 
 
 
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -83,6 +83,29 @@ function protectRoute( next ) {
   }
 }
 
+/**
+ * @param email, password
+ */
+function updateUserSettings( settings ) {
+  //get user id
+  console.log('auth.service.js updateUserSettings: ', settings);
+  return new Promise(resolve => {
+    resolve({
+      settings: settings
+    });  
+  });  
+  //handle settings
+  // return Vue.http.post('http://localhost:3003/data/user', {userName: email, pass: password} )
+  // .then(res => res.json())
+  // .then(({token, user}) => {
+  //   console.log('Signedin user:', user);
+  //   setSession(token, user);
+  //   return user;
+  // })
+
+}
+
+
 export default {
   signin,
   signup,
@@ -90,4 +113,5 @@ export default {
   setSession,
   isLoggedIn,
   protectRoute,
+  updateUserSettings,
 }
